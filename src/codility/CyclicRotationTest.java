72ab21e211225d5a66aaa5a80fdc2c83a3b375e3
marciogj <marciogj@gmail.com>
2018-08-11 15:14:05 -0300

    Added CyclicRotation 87% on codility

---
+++ src/codility/CyclicRotationTest.java
@@ -0,0 +1,52 @@
+package codility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CyclicRotationTest {
+	
+	@Test
+	public void test() {
+		CyclicRotation rotation = new CyclicRotation();
+		int[] a = {1,2,3,4,5};
+		int[] b = {5,1,2,3,4};
+		int k = 1;
+		//assertArrayEquals(b, rotation.solution(a, k));
+		
+		
+		a = new int[] {3, 8, 9, 7, 6};
+		b = new int[] {9, 7, 6, 3, 8};
+		k = 3;
+		assertArrayEquals(b, rotation.solution(a, k));
+		
+		
+		a = new int[] {1, 2, 3, 4};
+		b = new int[] {1, 2, 3, 4};
+		k = 4;
+		assertArrayEquals(b, rotation.solution(a, k));
+		
+		a = new int[] {1};
+		b = new int[] {1};
+		k = 6;
+		assertArrayEquals(b, rotation.solution(a, k));
+		
+
+		a = null;
+		b = null;
+		k = 6;
+		assertArrayEquals(b, rotation.solution(a, k));
+		
+		a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+		b = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+		k = 15;
+		assertArrayEquals(b, rotation.solution(a, k));
+		
+		
+		a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		b = new int[] {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		k = 10;
+		assertArrayEquals(b, rotation.solution(a, k));
+		 
+	}
+}
