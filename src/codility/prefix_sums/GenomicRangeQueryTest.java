9d27324f1e3e51468b9a9eefde010850dfbfa3bf
marciogj <marciogj@gmail.com>
2018-10-14 09:18:13 -0300

    Adding GenomicRangeQuery with only 62% correctness - performance not achieved

---
+++ src/codility/prefix_sums/GenomicRangeQueryTest.java
@@ -0,0 +1,36 @@
+package codility.prefix_sums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GenomicRangeQueryTest {
+
+	@Test
+	public void test() {
+		GenomicRangeQuery grq = new GenomicRangeQuery();
+		
+		
+		String S;
+		int[] P;
+		int[] Q;
+		int[] solution;
+		
+		
+		S = "CAGCCTA";
+		P = new int[] {2,5,0};
+		Q = new int[] {4,5,6};
+		solution = new int[] {2,4,1};
+		assertArrayEquals(solution, grq.solution(S, P, Q));
+		
+		S = "AC";
+		P = new int[] {0,0,1};
+		Q = new int[] {0,1,1};
+		solution = new int[] {1,1,2};
+		assertArrayEquals(solution, grq.solution(S, P, Q));
+		
+		
+		
+	}
+
+}
